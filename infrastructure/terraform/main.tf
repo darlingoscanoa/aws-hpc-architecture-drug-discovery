868850669220a67cf2fcdd20a8e087eebee013c7
Darling Oscanoa <darling.oscanoa@gmail.com>
2025-04-08 13:12:50 -0400

    fix: Remove unsupported arguments from auto_shutdown module

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -105,8 +105,5 @@ module "auto_shutdown" {
   
   project_name = var.project_name
   environment  = var.environment
-  vpc_id       = module.vpc.vpc_id
-  subnet_id    = module.vpc.private_subnet_ids[0]
-  cluster_security_group_id = module.parallelcluster.cluster_security_group_id
   shutdown_hour = var.shutdown_hour
 }
