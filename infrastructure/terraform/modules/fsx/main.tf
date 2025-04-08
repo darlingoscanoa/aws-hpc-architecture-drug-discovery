54a49032b4fc73d423a3e4f53e6f6b533c6ccf45
Darling Oscanoa <darling.oscanoa@gmail.com>
2025-04-08 14:53:22 -0400

    fix: Change FSx deployment type to PERSISTENT_1 for S3 integration

--- infrastructure/terraform/modules/fsx/main.tf
+++ infrastructure/terraform/modules/fsx/main.tf
@@ -38,7 +38,8 @@ resource "aws_security_group" "fsx" {
 resource "aws_fsx_lustre_file_system" "hpc" {
   storage_capacity    = var.storage_capacity
   subnet_ids         = [var.subnet_id]
-  deployment_type    = "SCRATCH_2"
+  deployment_type    = "PERSISTENT_1"
+  per_unit_storage_throughput = 50
   security_group_ids = [aws_security_group.fsx.id]
 
   tags = {
