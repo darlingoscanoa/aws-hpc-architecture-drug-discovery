dde7eafc4edb30d08e927da0429725f39899ae49
Darling <darling.oscanoa@gmail.com>
2025-04-11 19:12:29 -0400

    Update route table associations for single AZ configuration

--- infrastructure/terraform/modules/vpc/main.tf
+++ infrastructure/terraform/modules/vpc/main.tf
@@ -105,13 +105,13 @@ resource "aws_route_table" "private" {
 
 # Route Table Associations
 resource "aws_route_table_association" "public" {
-  count          = 2
+  count          = 1
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
-  count          = 2
+  count          = 1
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
