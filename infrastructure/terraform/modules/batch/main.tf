07f233857ec32294fc3da89f4ef88637d3ad3274
darlingoscanoa <darlingoscanoa@gmail.com>
2025-05-12 13:12:27 -0400

    Fix: Change assignPublicIp from boolean to string in Batch job definition

--- infrastructure/terraform/modules/batch/main.tf
+++ infrastructure/terraform/modules/batch/main.tf
@@ -116,7 +116,7 @@ resource "aws_batch_job_definition" "inference" {
     executionRoleArn = aws_iam_role.batch_execution_role.arn
     jobRoleArn      = aws_iam_role.batch_job_role.arn
     networkConfiguration = {
-      assignPublicIp = true
+      assignPublicIp = "ENABLED"
     }
     portMappings = [
       {
